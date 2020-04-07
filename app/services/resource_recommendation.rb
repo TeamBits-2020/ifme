295e44fb5904ffe127ec20a651c194870a4bf0d3
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-06 21:30:25 -0700

    Match a moment name with resources according to tags

--- app/services/resource_recommendation.rb
+++ app/services/resource_recommendation.rb
@@ -6,10 +6,14 @@ class ResourceRecommendation
   end
 
   def resources
-    resources = JSON.parse(File.read(Rails.root.join('doc', 'pages', 'resources.json')))
+    all_resources = JSON.parse(File.read(Rails.root.join('doc', 'pages', 'resources.json')))
+    matched_resources = []
+    moment_name = @moment.name.split
+    all_resources.each do |resource|
+      unless (resource['tags'] & moment_name).empty?
+        matched_resources.push(resource)
+      end
+    end
+    matched_resources
   end
-
-
-
-
-end
+  end
