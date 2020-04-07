a65d35347dd638105536d1d0971601262752dabe
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-07 15:17:30 -0700

    Match tag with why and fix

--- app/services/resource_recommendation.rb
+++ app/services/resource_recommendation.rb
@@ -8,12 +8,22 @@ class ResourceRecommendation
   def resources
     all_resources = JSON.parse(File.read(Rails.root.join('doc', 'pages', 'resources.json')))
     matched_resources = []
+    moment_keywords = []
     moment_name = @moment.name.split
+    moment_keywords.push(moment_name)
+    moment_why = @moment.why.split
+    moment_keywords.push(moment_why)
+    moment_fix = @moment.fix.split
+    moment_keywords.push(moment_fix)
+    moment_keywords = moment_keywords.flatten
     all_resources.each do |resource|
-      unless (resource['tags'] & moment_name).empty?
+      unless (resource['tags'] & moment_keywords).empty?
         matched_resources.push(resource)
       end
     end
     matched_resources
   end
   end
+
+#the mathcing happen with just one word and in alphabetical order
+# flatten puts it in order
