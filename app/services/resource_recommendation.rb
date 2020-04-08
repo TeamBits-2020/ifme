f8592bf2c50d59b184d4f1b87b3dc3d03b24dd8f
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-07 21:31:24 -0700

    [#1713] match resource tag with moment category name and description

--- app/services/resource_recommendation.rb
+++ app/services/resource_recommendation.rb
@@ -1,7 +1,6 @@
-include PagesConcern
 
 class ResourceRecommendation
-
+    
     def initialize(moment)
       @moment = moment
     end
@@ -13,6 +12,13 @@ class ResourceRecommendation
       moment_name = @moment.name.split
       moment_why = @moment.why.split
       moment_fix = @moment.fix.split
+      @moment.categories.each do |category|
+        moment_keywords.push(category['name'].split)
+      end
+      @moment.categories.each do |category|
+        category_description = category['description']
+        moment_keywords.push(category_description.split)
+      end
       moment_keywords.push(moment_name, moment_why, moment_fix)
       moment_keywords = moment_keywords.flatten
       moment_keywords = moment_keywords.map(&:downcase)
