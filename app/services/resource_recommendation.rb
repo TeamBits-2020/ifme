52004c6b73c8c174d2a41895c830ef38110f5b8a
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-07 17:12:15 -0700

    Extract strategy names and compare to tags

--- app/services/resource_recommendation.rb
+++ app/services/resource_recommendation.rb
@@ -15,6 +15,12 @@ class ResourceRecommendation
     @moment.categories.each do |category|
       moment_keywords.push(category['name'])
     end
+    @moment.categories.each do |mood|
+      moment_keywords.push(mood['name'])
+    end
+    @moment.strategies.each do |strategy|
+      moment_keywords.push(strategy['name'])
+    end
     moment_keywords.push(moment_name, moment_why, moment_fix)
     moment_keywords = moment_keywords.flatten
     moment_keywords = moment_keywords.map(&:downcase)
