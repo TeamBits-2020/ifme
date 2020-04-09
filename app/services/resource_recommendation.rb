ad42eb93706f469cb40424bff41f4e04516d27f8
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-08 22:11:30 -0700

    [#1713] shortened lines of code as per rubocop

--- app/services/resource_recommendation.rb
+++ app/services/resource_recommendation.rb
@@ -28,13 +28,16 @@ class ResourceRecommendation
     matched_resources = []
     moment_keywords = []
     @moment.categories.each do |category|
-      moment_keywords.push(category['name'].split, html_clean(category['description']).split)
+      moment_keywords.push(category['name'].split,
+                           html_clean(category['description']).split)
     end
     @moment.moods.each do |mood|
-      moment_keywords.push(mood['name'].split, html_clean(mood['description']).split)
+      moment_keywords.push(mood['name'].split,
+                           html_clean(mood['description']).split)
     end
     @moment.strategies.each do |strategy|
-      moment_keywords.push(strategy['name'].split, html_clean(strategy['description']).split)
+      moment_keywords.push(strategy['name'].split,
+                           html_clean(strategy['description']).split)
     end
     moment_keywords.push(moment_name, moment_why, moment_fix)
     moment_keywords = moment_keywords.flatten
