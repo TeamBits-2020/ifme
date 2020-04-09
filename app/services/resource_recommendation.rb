129dd714281a305b45b370bb429254fab1d6f429
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-08 21:57:11 -0700

    Refactor category keywords loop into a method

--- app/services/resource_recommendation.rb
+++ app/services/resource_recommendation.rb
@@ -3,34 +3,32 @@
 class ResourceRecommendation
   def initialize(moment)
     @moment = moment
+    @moment_keywords = []
   end
 
   def resources
     matched_resources = []
-    moment_keywords = []
-    @moment.categories.each do |category|
-      moment_keywords.push(category['name'].split,
-                           strip_tags(category['description']).split)
-    end
+    category_keywords
+
     @moment.moods.each do |mood|
-      moment_keywords.push(mood['name'].split,
+      @moment_keywords.push(mood['name'].split,
                            strip_tags(mood['description']).split)
     end
     @moment.strategies.each do |strategy|
-      moment_keywords.push(strategy['name'].split,
+      @moment_keywords.push(strategy['name'].split,
                            strip_tags(strategy['description']).split)
     end
-    moment_keywords.push(moment_name, moment_why, moment_fix)
-    moment_keywords = moment_keywords.flatten
-    moment_keywords = moment_keywords.each do |keyword|
+    @moment_keywords.push(moment_name, moment_why, moment_fix)
+    @moment_keywords = @moment_keywords.flatten
+    @moment_keywords = @moment_keywords.each do |keyword|
       keyword.gsub!(%r{([_@#!%()\-=;><,{}\~\[\]\./\?\"\*\^\$\+\-]+)}, '')
     end
-    moment_keywords = moment_keywords.map(&:downcase)
+    @moment_keywords = @moment_keywords.map(&:downcase)
     all_resources.each do |resource|
       tags = resource['tags'].map do |tag|
         tag.split('_')
       end
-      unless (tags.flatten & moment_keywords).empty?
+      unless (tags.flatten & @moment_keywords).empty?
         matched_resources.push(resource)
       end
     end
@@ -55,5 +53,12 @@ class ResourceRecommendation
 
   def strip_tags(str)
     ActionController::Base.helpers.strip_tags(str)
+  end
+
+  def category_keywords
+    @moment.categories.each do |category|
+      @moment_keywords.push(category['name'].split,
+                            strip_tags(category['description']).split)
+    end
   end
 end
