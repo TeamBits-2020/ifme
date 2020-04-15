2d93d594a39725c1b9b7eb8c0424576a3e926178
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-14 17:13:26 -0700

    Turn keywords into a let symbol to avoi repetition

--- spec/services/moment_keywords_spec.rb
+++ spec/services/moment_keywords_spec.rb
@@ -6,22 +6,20 @@ describe MomentKeywords do
                                      why: "More testing content self-care.",
                                      fix: "Text tested is @Teachers!!") }
 
+  let(:keywords) { MomentKeywords.new(moment).extract_moment_keywords }
 
   it 'downcases words' do
-    keywords = MomentKeywords.new(moment).extract_moment_keywords
     expect(keywords).to include("addiction")
   end
 
   it 'separates hyphenated words' do
-    keywords = MomentKeywords.new(moment).extract_moment_keywords
     expect(keywords).to include("self", "care")
   end
 
   it 'removes special characters' do
-    keywords = MomentKeywords.new(moment).extract_moment_keywords
     expect(keywords).to include("teachers")
   end
 end
 
 
-#let or subject
+
