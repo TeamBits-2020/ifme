ad522be305bb912e6c89dd4db6b3d5def3e44cd3
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-15 14:53:53 -0700

    [#1713] test extracting all resources

--- spec/services/resource_recommendation_spec.rb
+++ spec/services/resource_recommendation_spec.rb
@@ -5,20 +5,35 @@ describe ResourceRecommendation do
                                        name: "Test ADDICTION",
                                        why: "More testing content self-care.",
                                        fix: "Text tested is @Teachers!!") }
-    let(:keywords) { MomentKeywords.new(moment).extract_keywords } 
+     
     let(:resources) { ResourceRecommendation.new(moment).resources }
+    
 
     it 'initiates the extraction of moment_keywords' do
-        expect(keywords).to include("addiction")
+        expect(@moment_keywords).to eq(MomentKeywords.new(moment).extract_keywords)
     end
 
-    it 'extracts all the resource keywords from tags' do
+    it 'extracts all the resources' do
+        available_resource = [ {
+            "name"=> "Insight Timer",
+            "link"=> "https://insighttimer.com",
+            "tags"=> ["free", "meditation", "teachers", "self_care", "android", "ios"],
+            "languages"=> ["en"]
+        }  ]
+        allow(JSON).to receive(:parse) { available_resource }
+        expect(resources).to eq(available_resource)
+    end
+
+    it 'gets all the keywords from resource tags' do
     end
 
     it 'separates hyphenated or underscored words' do
     end
 
     it 'matches all the moment_keywords with the resource keywords' do
+    end
+
+    it 'does not display unmatched keyword links' do
     end
 
     it 'displays the links of resources based on matched keyword' do
