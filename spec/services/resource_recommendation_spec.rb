585b3140c7e5489ed02229f3d52a4a5982340a03
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-15 18:23:41 -0700

    [#1713] test for underscored tags

--- spec/services/resource_recommendation_spec.rb
+++ spec/services/resource_recommendation_spec.rb
@@ -1,51 +1,43 @@
 
 describe ResourceRecommendation do
-    let(:user) { create(:user) }
     subject(:moment) {FactoryBot.build(:moment,
                                        name: "Test ADDICTION",
                                        why: "More testing content self-care.",
-                                       fix: "Text tested is SELF-INJURY@") }
+                                       fix: "Text tested is @Teachers!!") }
      
-    let(:matches) { ResourceRecommendation.new(moment).resources }
-    describe 'testing .resource method for ResourceRecommendation class' do
+    let(:recommended_resources) { ResourceRecommendation.new(moment).resources }
+    describe 'test .resource method for ResourceRecommendation class' do
         it 'returns recommended resources matched with moment keywords' do
             matched_resource = [ {
                 "name"=> "Insight Timer",
                 "link"=> "https://insighttimer.com",
-                "tags"=> ["free", "meditation", "teachers", "self_care", "android", "ios"],
+                "tags"=> ["testing"],
                 "languages"=> ["en"]
             }  ]
             allow(JSON).to receive(:parse) { matched_resource }
-            expect(matches).to eq(matched_resource)
+            expect(recommended_resources).to eq(matched_resource)
         end
 
         it 'returns no recommended resources when no moment keywords match with tag keywords' do
             unmatched_resource = [ {
               "name"=> "Aloe Bud",
               "link"=> "https://aloebud.com",
-              "tags"=> ["self-care","anonymous", "ios", "communities"],
+              "tags"=> ["anonymous"],
               "languages"=> ["en"]
             }  ]
             allow(JSON).to receive(:parse) { unmatched_resource }
-            expect(matches).not_to eq(unmatched_resource)
+            expect(recommended_resources).not_to eq(unmatched_resource)
         end
 
-        it 'gets all the keywords from resource tags' do
-        end
-
-        it 'separates hyphenated or underscored words the same as moment keywords' do
-        end
-
-        it 'matches all the moment_keywords with the resource keywords' do
-        end
-
-        it 'does not display unmatched keyword links' do
-        end
-
-        it 'displays the links of resources based on matched keyword' do
-        end
-
-        it 'limits the display of links to three' do
+        it 'matches underscored tags' do
+            matched_resource = [ {
+                "name"=> "Insight Timer",
+                "link"=> "https://insighttimer.com",
+                "tags"=> ["self_care"],
+                "languages"=> ["en"]
+            }  ]
+            allow(JSON).to receive(:parse) { matched_resource }
+            expect(recommended_resources).to eq(matched_resource)
         end
 
     end
