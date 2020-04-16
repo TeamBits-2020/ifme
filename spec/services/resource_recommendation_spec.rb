93eee5e0708bb2490ba4f744fa291f26114424b2
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-15 18:29:09 -0700

    Test clean up

--- spec/services/resource_recommendation_spec.rb
+++ spec/services/resource_recommendation_spec.rb
@@ -8,29 +8,13 @@ describe ResourceRecommendation do
 
   describe 'A test for resources method from ResourceRecommendation class' do
     it 'returns recommended resources based on moment keywords' do
-      available_resource = [ {
-                                   'name'=> "LifeSIGNS: Self Injury Guidance & Network Support (UK)",
-                                   'link'=> "http://www.lifesigns.org.uk/",
-                                   'tags'=> ["self_injury", "blog", "books", "communities"],
-                                   'languages'=> ["en"]
-                               },
-                             {
-                                 'name'=> "Self-Injury Outreach and Support (Canada)",
-                                 'link'=> "http://sioutreach.org/",
-                                 'tags'=> ["self_injury", "education"],
-                                 'languages'=> ["en"]
-                             }]
+      available_resource = [{'tags'=> ["self_injury"]}]
       allow(JSON).to receive(:parse) { available_resource }
       expect(resources).to eq(available_resource)
     end
 
     it 'does not return any matched resources' do
-      available_resource2 = [ {
-                                  'name'=> 'Aloe Bud',
-                                  'link'=> 'https://aloebud.com',
-                                  'tags'=> ['self_care', 'anonymous', 'ios', 'communities'],
-                                  'languages'=> ['en']
-                                } ]
+      available_resource2 = [{'tags'=> ['self_care', 'anonymous', 'ios', 'communities']}]
       allow(JSON).to receive(:parse) { available_resource2 }
       expect(resources).not_to eq(available_resource2)
     end
