11ef041a523c73c964dffe3de44a513e4ef57c4e
Aline Ribeiro <alinear.mail@gmail.com>
2020-07-15 09:58:43 -0700

    Roll back migration and add index to link

--- app/models/external_resource.rb
+++ app/models/external_resource.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ExternalResource < ApplicationRecord
   validates :name, presence: true
-  validates :link, presence: true, uniqueness: true
+  validates :link, presence: true, uniqueness: true, index: { unique: true }
   validates :languages, presence: true
 end
