c1ad30f3de834480197bb8d443a8f50bea76a4d7
Aline Ribeiro <alinear.mail@gmail.com>
2020-07-15 10:58:34 -0700

    Test external_resource model

--- spec/models/external_resource_spec.rb
+++ spec/models/external_resource_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe ExternalResource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:external_resource) { FactoryBot.build(:external_resource) }
+
+  it ' has a valid factory' do
+    expect(external_resource.validate!).to eq(true)
+  end
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:link) }
+  it { is_expected.to validate_uniqueness_of(:link) }
+  it { is_expected.to validate_presence_of(:languages) }
 end
